6e9d3357946e3e94a9538d45a2dcd9f6cb5ae28e
machinedao <machinedao@gmail.com>
2015-02-26 00:11:08 +0800

    add java beginning project
    hello world project

---
+++ LearnJava/JavaBasic/src/org/machinedao/zz/Demo.java
@@ -0,0 +1,23 @@
+package org.machinedao.zz;
+
+public class Demo {
+	public static void main(String[] args){
+		class Student{
+			String name;
+			int age;
+			float score;
+			
+			void Say(){
+				System.out.println(name + "的年龄是 " + age + ",成绩是" + score);
+			}
+		}
+		
+		Student stu1 = new Student();
+		stu1.name = "小明";
+		stu1.age = 15;
+		stu1.score = 92.5f;
+		stu1.Say();
+		
+	}
+
+}
