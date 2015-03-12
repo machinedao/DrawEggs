859a51fb64f6a7e7d8ccd0606e14e6fe059d3cfd
codingames <codingames@gmail.com>
2015-03-12 18:48:16 +0800

    java basic - generic - wildcard
    泛型 通配符

---
+++ LearnJava/JavaBasic/src/org/machinegamez/zz/TryGenericWildcard.java
@@ -0,0 +1,40 @@
+package org.machinegamez.zz;
+
+public class TryGenericWildcard {
+
+	public static void main(String[] args) {
+		Point<Integer, Integer> p1 = new Point<Integer, Integer>();
+		p1.setX(10);
+		p1.setY(20);
+		pointPoint(p1);
+		
+		Point<String, String> p2 = new Point<String, String>();
+		p2.setX("东京180度");
+		p2.setY("北纬30度");
+		pointPoint(p2);
+
+	}
+	
+	public static void pointPoint(Point<?,?> p) {	// 使用通配符
+		System.out.println("This point is: " + p.getX() + ", " + p.getY());
+	}
+
+}
+
+class Point<T1, T2> {
+	T1 x;
+	T2 y;
+	
+	public T1 getX(){
+		return x;
+	}
+	public void setX(T1 x) {
+		this.x = x;
+	}
+	public T2 getY() {
+		return y;
+	}
+	public void setY(T2 y) {
+		this.y = y;
+	}
+}
